60db9b8cf729e8894e77b0ff45c1cd52eb32eb52
igorovic <hello@dyve.dev>
2021-09-09 10:30:03 +0200

    limit paths

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -5,3 +5,17 @@ export default function DynamicPage(props) {
     </div>
   );
 }
+export async function getStaticProps(context) {
+  return {
+    props: { message: "some post" },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = ["a", "b"];
+  const paths = posts.map((post) => ({
+    params: { slug: post },
+  }));
+
+  return { paths, fallback: false };
+}
