9da80aac26bd0f6aa10853cc87dd9eb1b39b9327
igorovic <hello@dyve.dev>
2021-09-09 09:30:05 +0200

    dynamic route part 2

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,9 +1,16 @@
-export default function DynamicPage() {
+export default function DynamicPage(props) {
   return (
     <div>
       <p>dynamic page</p>
+      <p>{props.message}</p>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: { message: "dynamic page part 2" }, // will be passed to the page component as props
+  };
 }
 
 export async function getStaticPaths() {
